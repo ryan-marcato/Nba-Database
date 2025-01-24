9ef324ba3bd03e7558c43cc60d23fc1cbf69377c
Ryan Marcato <ryanh2402@gmail.com>
2025-01-24 18:42:37 -0300

    new info

--- db/create_tb/jogadores.sql
+++ db/create_tb/jogadores.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS tb_jogadores (
     id_jogador VARCHAR(100) PRIMARY KEY,
     nome_jogador VARCHAR(100) NOT NULL,
-    time_jogador VARCHAR(3)
+    time_jogador VARCHAR(10)
 )
