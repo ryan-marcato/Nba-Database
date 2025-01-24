61e8308866c4cad1529d11169a8f2bcc8c4f9150
Ryan Marcato <ryanh2402@gmail.com>
2025-01-24 13:45:35 -0300

    add func query insert

--- utils/db_utils/execute_query.py
+++ utils/db_utils/execute_query.py
@@ -1,7 +1,7 @@
 from utils.db_utils.conection_db import create_conection
 from sqlalchemy import text, exc
 
-def query_execute(query):
+def query_execute_tb(query):
     try:
         conexao = create_conection()
         if conexao is None:
@@ -13,4 +13,19 @@ def query_execute(query):
             print("Query executadada com sucesso")
     except exc.SQLAlchemyError as e:
         print(f"Nao foi possivel executar a query: {e}")
+
+#Funçao para insert e update
+#commit para confirmar a transação
+def query_insert_execute(query):
+    try:
+        conexao = create_conection()
+        if conexao is None:
+            print("Nao foi possivel abrir conexao")
+            return
         
+        with conexao:
+            conexao.execute(text(query))
+            conexao.commit() 
+            print("Query executada com sucesso")
+    except exc.SQLAlchemyError as e:
+        print(f"Nao foi possivel executar a query: {e}")
