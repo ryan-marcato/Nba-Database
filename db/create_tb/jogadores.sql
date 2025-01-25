2bb50979e8dce0971327201658538064a0a0fa9a
Ryan Marcato <ryanh2402@gmail.com>
2025-01-25 15:34:20 -0300

    new fk

--- db/create_tb/jogadores.sql
+++ db/create_tb/jogadores.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS tb_jogadores (
     id_jogador VARCHAR(100) PRIMARY KEY,
     nome_jogador VARCHAR(100) NOT NULL,
-    time_jogador VARCHAR(10)
+    time_jogador VARCHAR(10),
+    CONSTRAINT fk_sigla_time FOREIGN KEY (time_jogador) REFERENCES tb_times (sigla_time)
 )
